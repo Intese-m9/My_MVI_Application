9d5910a70b392e8dd1e597f553ba465583ba1dfa
Ivan Balandin <9527886@mail.ru>
2024-02-17 21:30:50 +0400

    Create preview in CatsCard.kt

--- app/src/main/java/com/example/mymviapplication/presentation/compose/catsScreen/CatsCard.kt
+++ app/src/main/java/com/example/mymviapplication/presentation/compose/catsScreen/CatsCard.kt
@@ -8,9 +8,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.example.mymviapplication.R
 import com.example.mymviapplication.data.model.modelCat.Data
+
 
 @Composable
 fun CatsCard(cat: Data) {
@@ -21,9 +25,23 @@ fun CatsCard(cat: Data) {
             .fillMaxHeight()
     ) {
         Text(
-            modifier = Modifier
-                .padding(35.dp),
-            text = cat.fact, color = colorResource(id = R.color.black)
+            modifier = Modifier.padding(35.dp),
+            text = cat.fact,
+            color = colorResource(id = R.color.black)
         )
     }
+}
+
+class PreviewParamentData : PreviewParameterProvider<Data> {
+    override val values = sequenceOf(
+        Data("Elise", 1), Data("Frank", 2), Data("Julia", 3)
+    )
+}
+
+@Preview
+@Composable
+private fun CatsPreview(
+    @PreviewParameter(PreviewParamentData::class) data: Data
+) {
+    CatsCard(cat = data)
 }
