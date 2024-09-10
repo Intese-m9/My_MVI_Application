20167868cc0f8d59058cb14cf95f28f958b4174a
Ivan Balandin <9527886@mail.ru>
2024-09-10 11:30:26 +0400

    Update DataModule. Provides to Binds.

--- app/src/main/java/com/example/mymviapplication/di/DataModule.kt
+++ app/src/main/java/com/example/mymviapplication/di/DataModule.kt
@@ -2,18 +2,16 @@ package com.example.mymviapplication.di
 
 import com.example.mymviapplication.data.repository.MainRepositoryImpl
 import com.example.mymviapplication.domain.MainRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataModule {
-    @Provides
+abstract class DataModule {
+    @Binds
     @Singleton
-    fun provideRepository(): MainRepository {
-        return MainRepositoryImpl()
-    }
-}
+  abstract fun provideRepository(impl:MainRepositoryImpl): MainRepository
+  }
