cd135c939c10df1b894016e44f8f132a922f07df
Ivan Balandin <9527886@mail.ru>
2024-09-10 13:35:41 +0400

    Update DataModule. Provides to Binds.

---
+++ app/src/main/java/com/example/mymviapplication/data/repository/MainRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package com.example.mymviapplication.data.repository
+
+import com.example.mymviapplication.data.api.RetrofitBuilder
+import com.example.mymviapplication.domain.MainRepository
+import javax.inject.Inject
+
+class MainRepositoryImpl @Inject constructor():MainRepository {
+/*
+    suspend fun getUsers(page:Int,limit: Int) = RetrofitBuilder.apiService.getUsers(page,limit)
+*/
+     override suspend fun getCats() = RetrofitBuilder.apiCatService.getCats()
+     override fun getUserPagination() = RetrofitBuilder.getPagingAllPost()
+}
